e7dffa6a689f4dd87cdc9daa9e4951deebc1795e
2rpadala <rajvja520002@gmail.com>
2023-04-25 21:31:40 +0530

    imageid

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+output "aws" {
+  value = data.aws_ami.centos.image_id
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -6,6 +16,7 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
 output "frontend" {
   value = aws_instance.frontend.public_ip
 }
