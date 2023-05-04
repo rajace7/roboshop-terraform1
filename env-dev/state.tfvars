41fce5ca7f885dbaf1b487869e60cdda30cf8b56
2rpadala <rajvja520002@gmail.com>
2023-05-04 11:53:30 +0530

    state

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-rpadala"
+key    = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-rpadala"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
