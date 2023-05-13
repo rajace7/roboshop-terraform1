44c6c2e632dc1d08bdb6344753999b294aedd41d
2rpadala <rajvja520002@gmail.com>
2023-05-13 10:52:02 +0530

    payment ansible

--- module/main.tf
+++ module/main.tf
@@ -55,8 +55,37 @@ resource "aws_iam_role" "role" {
   })
 
   tags = {
-    tag-key = "${var.component_name}-${var.env}-rolee"
+    tag-key = "${var.component_name}-${var.env}-role"
   }
+}
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:487992542253:parameter/${var.env}.${var.component_name}.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
 }
 
 
