234725d5cfda92cf5949195ac42d08e90ff060e7
2rpadala <rajvja520002@gmail.com>
2023-05-13 10:05:23 +0530

    payment ansible

--- module/local.tf
+++ module/local.tf
@@ -10,7 +10,7 @@ locals {
 
   app_commands = [
   "sudo labauto ansible",
-    "ansible-pull -i localhost, -u https://github.com/rajace7/roboshop-ansible1.git -e role_name = ${var.component_name} -e env=${var.env}"
+    "ansible-pull -i localhost, -U https://github.com/rajace7/roboshop-ansible1.git -e role_name = ${var.component_name} -e env=${var.env}"
   ]
 
 }
