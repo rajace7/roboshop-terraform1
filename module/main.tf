1562dcc4cee413844aae1a6604c15a364f536bcd
2rpadala <rajvja520002@gmail.com>
2023-05-14 18:58:42 +0530

    pull ansible roboshop

--- module/main.tf
+++ module/main.tf
@@ -63,7 +63,7 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_role_policy" "ssm-ps-policy" {
+/*resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
 
@@ -91,7 +91,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
     ]
   })
 }
-
+*/
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "${var.component_name}-${var.env}-role"
   role = aws_iam_role.role.name
