a8b060ae713be8b2618d1221504aba4471ea887b
2rpadala <rajvja520002@gmail.com>
2023-06-04 13:58:32 +0530

    load balancer

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
 //variable "domain_name" {}
-//variable "domain_id" {}
+variable "domain_id" {}
 variable "kms_arn" {}
 
 variable "docdb" {}
